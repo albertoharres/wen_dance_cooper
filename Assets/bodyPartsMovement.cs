f02852719e500c07f76c3f328a836b4967249b6c
Alberto Harres <alberto@superuber.com>
2017-02-06 11:23:10 -0200

    aaaaarg!

---
+++ Assets/bodyPartsMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bodyPartsMovement : MonoBehaviour {
+
+    GameObject[] head;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        head = GameObject.FindGameObjectsWithTag("head");
+
+        foreach(GameObject obj in head)
+        {
+        }
+    }
+}
